ecebf82b61ec0df001aeb3d1472445457a8bd91a
Simon Gowing <simongowing1@gmail.com>
2021-02-03 16:29:42 +0100

    works page created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public/images'));
 
 app.get('/', function (request, response) {
     console.log(__dirname);
