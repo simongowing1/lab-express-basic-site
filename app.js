2e23848c4259384b7b716818a0d97ef2a83f11dc
Simon Gowing <simongowing1@gmail.com>
2021-02-03 16:55:43 +0100

    works page added, all links working

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-//app.use(express.static(__dirname + '/public/images'));
 
 app.get('/', function (request, response) {
-    console.log(__dirname);
-    //response.send('<h1>Home Page</h1>')
+    console.log('responding');
     response.sendFile(__dirname + '/views/homepage.html')
 })
 
 app.get('/about', function (req, res) {
     res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', function (req, res) {
+    res.sendFile(__dirname + '/views/works.html')
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/homepage.html')
 })
 
 app.listen(3000, () => {
